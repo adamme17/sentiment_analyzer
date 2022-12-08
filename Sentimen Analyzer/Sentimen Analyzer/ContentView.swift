93ba5dbaefa3f27177c63a9e79d0a91665f22601
Adam <adambokun@gmail.com>
2022-12-08 14:32:38 +0300

    init a project

---
+++ Sentimen Analyzer/Sentimen Analyzer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sentimen Analyzer
+//
+//  Created by Adam Bokun on 8.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sentimen Analyzer/Sentimen Analyzer/Sentimen_AnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sentimen_AnalyzerApp.swift
+//  Sentimen Analyzer
+//
+//  Created by Adam Bokun on 8.12.22.
+//
+
+import SwiftUI
+
+@main
+struct Sentimen_AnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
